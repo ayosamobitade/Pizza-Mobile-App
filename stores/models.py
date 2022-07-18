8de625f99d542a669e3ad660119a49f8c96e272e
UncleAyo <ayosamobitade@gmail.com>
2022-07-18 23:31:06 +0100

    some Updates

--- stores/models.py
+++ stores/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.core.validators import RegexValidator
 
+
 class Pizzeria_name(models.Model):
     pizzeria_name = models.CharField(max_length = 200, blank = False)
     street = models.CharField(max_length  = 400, blank=True)
