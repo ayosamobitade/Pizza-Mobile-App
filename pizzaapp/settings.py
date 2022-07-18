398866195d5c31212891a550b0442aa8d79e5749
UncleAyo <ayosamobitade@gmail.com>
2022-07-18 23:40:48 +0100

    some Updates

--- pizzaapp/settings.py
+++ pizzaapp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'stores.StoresConfig.apps',
+    'rest_framework',
+    'stores',
 ]
 
 MIDDLEWARE = [
--- stores/models.py
+++ stores/models.py
@@ -13,7 +13,7 @@ class Pizzeria_name(models.Model):
     webiste = models.CharField(max_length = 420, blank = True)
     phone_number = models.CharField(validators=[RegexValidator(regex=r'^\1?\d{9,10}$')], max_length= 10, blank = True)
     description = models.TextField(blank = True)
-    logo_image = models.TextField(upload_to='pizzariaImages', blank=True, default='pizzariaImages/pizzalogl.png')
+    logo_image = models.ImageField(upload_to='pizzariaImages', blank=True, default='pizzariaImages/pizzalogl.png')
     email = models.EmailField(max_length = 245, blank = True)
     active = models.BooleanField(default=True)
 
