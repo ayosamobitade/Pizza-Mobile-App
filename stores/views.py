b72155a78a0787dc8a4f418166ff0b3e05697582
UncleAyo <ayosamobitade@gmail.com>
2022-07-19 00:03:32 +0100

    some Updates

--- stores/views.py
+++ stores/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .serializers import PizzerraListSerializer
+from .models import Pizzeria
+
+class PizzeriaListAPIView(generics.ListAPIView):
+    queryser = Pizzeria.objects.all()
+    serializer_class = PizzerraListSerializer
