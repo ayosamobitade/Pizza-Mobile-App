0a384c899e242c1a8e049efe435256c18cb15ee3
UncleAyo <ayosamobitade@gmail.com>
2022-08-17 11:14:23 +0100

    some updates

--- stores/urls.py
+++ stores/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.PizzeriaListAPIView.as_view(), name = 'pizzeria_list'),
     path('<int:id>/', views.PizzeriaRetrieveAPIView.as_view(), name='pizzeria_detail'),
     path('create/', views.PizzeriaCreateAPIView.as_view(), name='pizzeria_create'),
-    path('update/', views.PizzeriaRetrieveUpdateView, name='pizzeria_update'),
+    path('update/', views.PizzeriaRetrieveUpdateView.as_view(), name='pizzeria_update'),
+    path('delete/', views.PizzeriaDestroyAPIView.as_view(), name='pizzeria_delete'),
 ]
